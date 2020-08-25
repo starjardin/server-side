a1b264fc9925e19ded81df21be71dbabf999680c
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-25 11:56:11 +0300

    added new script to run the nunjucks, and added new requires

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 console.log(`Greeting form Node and index.js! 🎺🎺`);
+
+const nunjucks = require('nunjucks');
 const express = require('express');
 //Let's test that Express is working as expected.
 //console.log(express);
 
 //Let's add a new app
 const app = express();
+nunjucks.configure({
+  autoescape: true,
+  express: app
+});
 
 //We want express to run on this port
 
@@ -13,8 +19,7 @@ const port = 3000;
 //we want listen method to run express, we'll need to pass it our `port`.
 
 app.get('/', (request, response) => {
-  console.log('Request: /');
-  response.sendFile(__dirname + '/index.html');
+  response.render('./views/index.html.njk');
 });
 
 app.listen(port, () => {
