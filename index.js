9c3f95d4d8b483c1d4aa029ee24690af3c5c7f54
Tantely Andrianarivola <tantely.and@onja.org>
2020-08-25 10:56:56 +0300

    added scripts to run express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log(`Greeting form Node and index.js! 🎺🎺`)
+console.log(`Greeting form Node and index.js! 🎺🎺`);
+const express = require('express');
+//Let's test that Express is working as expected.
+//console.log(express);
+
+//Let's add a new app
+const app = express();
+
+//We want express to run on this port
+
+const port = 3000;
+
+//we want listen method to run express, we'll need to pass it our `port`.
+
+app.get('/', (request, response) => {
+  console.log('Request: /');
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`app is running here: http://localhost:${port}`);
+});
